6a595116d5c113ff6a2f55ccc98b27a1922f4dc1
wren <33828481+skrenata@users.noreply.github.com>
2019-03-13 15:30:06 +0000

    Update sample_question_run.R

--- sample_question_run.R
+++ sample_question_run.R
@@ -10,3 +10,4 @@ solveQuestion(r = 10, k = 9, w = 1, b = 1, m = 10)
 
 ## i would recommend leaving r = 10 or at another "large" number, if you choose
 ## a small value for r it can get difficult to see the solution trajectory
+## note: the green dot marks the end point of the trajectory
